375fbe54636d9e3c0a16cd02859bcde69a795d73
Farhan Zaman <fzaman2258@bths.edu>
2021-01-08 18:22:12 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {MenuItem, Select, FormControl,} from '@material-ui/core';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>COVID-19 Tracker</h1>
+      <FormControl className = "app_dropdown">
+        <Select
+          variant = 'outlined'
+          value = 'abc' >
+          <MenuItem value = 'worldwide'>Option 1</MenuItem>
+          <MenuItem value = 'worldwide'>Option 2</MenuItem>
+          <MenuItem value = 'worldwide'>Option 3</MenuItem>
+          <MenuItem value = 'worldwide'>Option 4</MenuItem>
+        </Select> 
+      </FormControl>
     </div>
   );
 }
