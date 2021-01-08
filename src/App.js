3737c0343d0ef12bd8a0fc29f94dcb409adc8ed8
Mohammad Saif <saminsaif29@gmail.com>
2021-01-08 18:44:49 -0500

    This should work

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import './App.css';
-import {MenuItem, Select, FormControl,} from '@material-ui/core';
+import "./App.css";
+import { MenuItem, Select, FormControl } from "@material-ui/core";
 function App() {
   return (
     <div className="app">
       <h1>COVID-19 Tracker</h1>
-      <FormControl className = "app_dropdown">
-        <Select
-          variant = 'outlined'
-          value = 'abc' >
-          <MenuItem value = 'worldwide'>Option 1</MenuItem>
-          <MenuItem value = 'worldwide'>Option 2</MenuItem>
-          <MenuItem value = 'worldwide'>Option 3</MenuItem>
-          <MenuItem value = 'worldwide'>Option 4</MenuItem>
-        </Select> 
+      <FormControl className="app_dropdown">
+        <Select variant="outlined" value="abc">
+          <MenuItem value="worldwide">Option 1</MenuItem>
+          <MenuItem value="worldwide">Option 2</MenuItem>
+          <MenuItem value="worldwide">Option 3</MenuItem>
+          <MenuItem value="worldwide">Option 4</MenuItem>
+        </Select>
       </FormControl>
     </div>
   );
