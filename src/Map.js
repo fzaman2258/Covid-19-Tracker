7177387ba7422a5de36356e941d4d0d152648d30
fzaman2258 <fzaman2258@bths.edu>
2021-01-18 00:02:01 -0500

    Added most functionality and UI features

---
+++ src/Map.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Map.css';
+import { MapContainer as LeafletMap, TileLayer} from "react-leaflet";
+import {showDataonMap} from "./util";
+
+function Map({countries, casesType, center,zoom}) {
+    return (
+        <div className="map">
+           <LeafletMap center={center} zoom={zoom} scrollWheelZoom={true} >
+               <TileLayer 
+                    url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution= '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+               />
+            {showDataonMap(countries,casesType)}
+           </LeafletMap>
+        </div>
+    );
+}
+
+export default Map;
